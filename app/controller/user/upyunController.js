3e937c124f11d1e4e3702116f490c512e32cf470
Joey <jiangjun12321@sina.com>
2018-04-21 18:20:03 +0800

    image token

--- app/controller/user/upyunController.js
+++ app/controller/user/upyunController.js
@@ -3,13 +3,35 @@
 const Controller = require('./baseController');
 const tools = require('upyun/tools')
 const utils = require('upyun/upyun/utils')
-
 class UpYunController extends Controller {
+  // 得到文件上传的路径
+  // 必填 role roleId page filename 
   async getImgToken() {
-    const {ctx} = this
+    const { ctx } = this
     const { params } = ctx.request.body;
 
-    const {filePath} = params
+    // role: user admin
+    // roleId: 在 role 是 admin 或者 user 的 id
+    // page: 所在的页面 可能是 editPage
+    const { role, roleId, source, sourceId, fileName, page } = params
+
+    const paramRule = {
+      role: { type: 'string', require: true, allowEmpty: false },
+      fileName: { type: 'string', require: true, allowEmpty: false },
+      page: { type: 'string', require: true, allowEmpty: false },
+      source: { type: 'string', require: false, allowEmpty: true },
+    };
+
+    // role: role,
+    // roleId: this.context.store.getState()[role].payload.id,
+    // source: source || 'temporary',
+    // sourceId: id || 'layout',
+
+    const uniqFileName = utils.md5sum(fileName + (Math.random().toString()))
+    let filePath = `/${page}/${role}/${roleId}${source ? ('/' + source) : ''}${sourceId ? ('/' + sourceId) : ''}/${uniqFileName}`
+    if (!this.validate(paramRule, params)) return;
+
+
     const secret = this.app.config.upyun.key
     const saveKey = this.app.config.upyun.savePathPrefix + filePath // 文件保存路径
     const imgUrl = this.app.config.upyun.urlPrefix + saveKey
@@ -21,8 +43,8 @@ class UpYunController extends Controller {
     }
     let policy = tools.policy(opts)
     let token = utils.md5sum(policy + '&' + secret)
-    this.success({token: token, policy: policy, imgUrl: imgUrl,saveKey: saveKey});
-  }  
+    this.success({ token: token, policy: policy, imgUrl: imgUrl, saveKey: saveKey });
+  }
 }
 
 module.exports = UpYunController;
--- config/config.local.js
+++ config/config.local.js
@@ -18,4 +18,15 @@ module.exports = {
   jwt_secret: "XInJiZhengBAWanDEFEIQI041418",
   app_key: "yOnGShaKeyHaoNe3387",
   domian: 'localhost',
+  
+  upyun: {
+    bucket: 'blog-src',
+    key: 'ZvmSM4XlWxmvJ6th7K9HR2BjXH0=',
+    admin: 'admin2', 
+    password: 'jj930328',
+    urlPrefix: 'http://blog-src.b0.upaiyun.com', // 图片上传成功后的地址前缀
+    // saveKey: '/taohe/development',
+    savePathPrefix: '/taohe/dev', // 保存图片的环境, 生产模式应为 production
+  }
+
 };
